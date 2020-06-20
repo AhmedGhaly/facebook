b8be1263df3c99be61e22dd9b2c243c1d23b2cfb
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2020-06-20 22:34:45 +0200

    update error message

--- app.js
+++ app.js
@@ -30,23 +30,14 @@ app.use(authRouter)
 
 //////////// error handling /////////////////////////
 app.use((req, res, next) => {
-    const err = new Error('this page is not found')
-    err.status = 404
-    next(err)
-})
-
-app.use((err, req, res, next) => {
-    console.log(err)
-    const status = err.status || 500
-    res.status(status).json({
-      message : err.message,
-      data : err.data
+    res.status(404).json({
+        err: 'this page is not found'
     })
 })
 //////////////////////////////////////////////////
 
 ////////////// server config /////////////////
-const port = process.env.PORT || '8080';
+const port = process.env.PORT;
 mongoose.connect(process.env.MONGOOSE_URL).then(() => {
     app.set('port', port);
     const server = http.createServer(app);
